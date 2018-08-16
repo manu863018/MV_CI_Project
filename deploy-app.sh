d917929aa99a692f202f1bebd611fca1a75f6e0e
Manuel Valdez <manuel.valdez@fundacion-jala.org>
2018-08-16 02:40:19 +0000

    Fix path of jar file.

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,12 +1,5 @@
-echo 'Stopping Tomcat service...'		
-sudo systemctl stop tomcat8
-sudo rm -f /var/lib/tomcat8/webapps/webapp-quickstart.war
-sudo rm -rf /var/lib/tomcat8/webapps/webapp-quickstart
+echo 'Remove jar directory'		
+sudo rm -f /home/ubuntu/jar_deploy
+sudo mkdir /home/ubuntu/jar_deploy
 
-echo 'Sending war file...'
-sudo cp -f /tmp/webapp-quickstart.war /var/lib/tomcat8/webapps/webapp-quickstart.war
-sudo rm /tmp/webapp-quickstart.war
-
-echo 'Starting Tomcat service...'
-sudo systemctl start tomcat8
 
